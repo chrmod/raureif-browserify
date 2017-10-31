9c9544386418632589f15523a4eaa1c9f492d0dc
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-10-31 19:30:34 +0100

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
-export default class {
+import Funnel from 'broccoli-funnel';
+import MergeTrees from 'broccoli-merge-trees';
+import glob from 'glob';
+import uppercamelcase from 'uppercamelcase';
+import watchify from 'broccoli-watchify';
 
-}
+const getOptions = ({ entries, outputFile, packageName, basePath }) => ({
+  browserify: {
+    entries,
+    paths: [`${basePath}/node_modules`],
+    standalone: packageName,
+    debug: false,
+  },
+  outputFile,
+  cache: true,
+});
+
+export default {
+  postBuild(tree, project) {
+    const basePath = project.path;
+    const packageManifest = project.pkg;
+    const packageName = uppercamelcase(packageManifest.name);
+    const buildForBrowser = Boolean(packageManifest.browser);
+    const outputTrees = [];
+
+    if (!buildForBrowser) {
+      return null;
+    }
+
+    outputTrees.push(
+      watchify(
+        tree,
+        getOptions({
+          basePath,
+          entries: ['./index.js'],
+          packageName,
+          outputFile: 'index.browser.js',
+        }),
+      ),
+    );
+
+    const testFiles = glob
+      .sync(`${basePath}/tests/browser/**/*-test.js`)
+      .map(filePath => `.${filePath.slice(basePath.length + 6)}`);
+
+    outputTrees.push(
+      watchify(
+        tree,
+        getOptions({
+          basePath,
+          entries: testFiles,
+          packageName,
+          outputFile: 'tests.browser.js',
+        }),
+      ),
+    );
+
+    return new MergeTrees(
+      outputTrees.map(
+        t =>
+          new Funnel(t, {
+            include: ['**/*.browser.js'],
+          }),
+      ),
+    );
+  },
+};
